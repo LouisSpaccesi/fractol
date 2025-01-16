9422d9cddd851d694bfdddf8051659df540dfff7
LouisSpaccesi <louisspaccesi@gmail.com>
2025-01-16 19:51:00 +0100

    Mandelbrot

--- fractol.c
+++ fractol.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fractol.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lospacce <lospacce@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lospacce < lospacce@student.42angouleme    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/26 11:11:02 by lospacce          #+#    #+#             */
-/*   Updated: 2025/01/16 16:27:17 by lospacce         ###   ########.fr       */
+/*   Updated: 2025/01/16 19:50:36 by lospacce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,15 @@
 
 int		ft_fractol(double c_re, double c_im)
 {
-	double	z_re = 0;
-	double	z_im = 0;
-	double	z_re2, z_im2;
-	int		iter = 0;
+	double	z_re;
+	double	z_im;
+	double	z_re2;
+	double	z_im2;
+	int		iter;
+	
+	z_re = 0;
+	z_im = 0;
+	iter = 0;
 
 	while (z_re * z_re + z_im * z_im <= 4 && iter < MAX_ITERATION)
 	{
@@ -55,19 +60,19 @@ void	render_fractal(t_img *img, t_data *data)
 	double	c_re;
 	double c_im;
 	int iter;
-	
+
 	y = 0;
 	while(y < WINDOW_HEIGHT)
 	{
 		x = 0;
 		while(x < WINDOW_WIDTH)
 		{
-			c_re = ((x - WINDOW_WIDTH / 2.0) * data->zoom)* 4.0 / WINDOW_WIDTH;
-			c_im = ((y - WINDOW_HEIGHT / 2.0) * data->zoom) * 4.0 / WINDOW_HEIGHT;
+			c_re = (x - WINDOW_WIDTH / 2.0) * 4.0 / WINDOW_WIDTH / data->zoom + data->mouse_re;
+            c_im = (y - WINDOW_HEIGHT / 2.0) * 4.0 / WINDOW_HEIGHT / data->zoom + data->mouse_im;
 			iter = ft_fractol(c_re, c_im);
 			color = 0x000000;
 			if (iter < MAX_ITERATION)
-				color = 0x00FF00 + (iter * 600 / MAX_ITERATION) * 256;
+				color = 0x0000FF + (iter * 600 / MAX_ITERATION) * 256;
 			img_pix_put(img, x, y, color);
 			x++;
 		}
@@ -115,9 +120,10 @@ int	render(t_data *data)
 int	main(void)
 {
 	t_data	data;
-	
+
 	data.zoom = 1.0;
-	
+	data.mouse_re = 0.0;
+	data.mouse_im = 0.0;
 	data.mlx_ptr = mlx_init();
 	if (data.mlx_ptr == NULL)
 	{
@@ -132,13 +138,11 @@ int	main(void)
 	}
 	data.img.mlx_img = mlx_new_image(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
 	data.img.addr = mlx_get_data_addr(data.img.mlx_img, &data.img.bpp, &data.img.line_len, &data.img.endian);
-	// mlx_mouse_get_pos(data.win_ptr, ft_zoom, (int)data.mouse_x, (int)data.mouse_y);
 	mlx_hook(data.win_ptr, 17, 0, &handle_cross, &data);
 	mlx_mouse_hook(data.win_ptr, ft_zoom, &data);
 	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data);
 	mlx_loop_hook(data.mlx_ptr, &render, &data);
 	mlx_loop(data.mlx_ptr);
-
 	mlx_destroy_image(data.mlx_ptr, data.img.mlx_img);
 	mlx_destroy_display(data.mlx_ptr);
 	free(data.mlx_ptr);
--- zoom.c
+++ zoom.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   zoom.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lospacce <lospacce@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lospacce < lospacce@student.42angouleme    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 11:52:26 by lospacce          #+#    #+#             */
-/*   Updated: 2025/01/16 17:00:17 by lospacce         ###   ########.fr       */
+/*   Updated: 2025/01/16 19:36:14 by lospacce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,37 +23,24 @@
 
 int ft_zoom(int button, int x, int y, t_data *data)
 {
-	// static double zoom;
 
-	// zoom = 1.0;
+	double long zoom_re;
+	double long zoom_im;
 
-	(void)y;
-	//int		mlx_mouse_get_pos(t_xvar *xvar, t_win_list *win, int *win_x_return, int *win_y_return);
-	double zoom_re = 1.0;
-	double zoom_im = 1.0;
-//	double zoom_im;
-		
-	// int mouse = mlx_mouse_get_pos(data->mlx_ptr, data->win_ptr, &x, &y);
-	// int mouse = mlx_mouse_move(data->mlx_ptr, data->win_ptr, x, y);
-	// printf("%f\n", zoom_re);
-	// printf("%d\n", y);
-
-	printf("-----\n");
-
-	zoom_re = (x - WINDOW_WIDTH / 2.0)* 4.0 / WINDOW_WIDTH;
-	zoom_im = (y - WINDOW_WIDTH / 2.0 )* 4.0 / WINDOW_WIDTH;
-	// printf("%f\n", zoom_re);
-
-	data->mouse_re = zoom_re;
-	data->mouse_im = zoom_im;
-	printf("%f", data->mouse_re);
-	printf("%f", data->mouse_im);
-
-	// mlx_mouse_get_pos(data->mlx_ptr, data->win_ptr, &x, &y);
+	zoom_re = (x - WINDOW_WIDTH / 2.0)* 4.0 / WINDOW_WIDTH / data->zoom + data->mouse_re;
+	zoom_im = (y - WINDOW_HEIGHT / 2.0)* 4.0 / WINDOW_HEIGHT / data->zoom + data->mouse_im;
     if (button == buttonfront)
+	{
 		data->zoom *= 1.1;
+		data->mouse_re = zoom_re - (zoom_re - data->mouse_re) / 1.1;
+		data->mouse_im = zoom_im - (zoom_im - data->mouse_im) / 1.1;
+	}
 	else if (button == buttonback)
+	{
 		data->zoom *= 0.9;
+		data->mouse_re = zoom_re - (zoom_re - data->mouse_re) / 0.9;
+		data->mouse_im = zoom_im - (zoom_im - data->mouse_im) / 0.9;
+	}
     return 0;
 }
 
